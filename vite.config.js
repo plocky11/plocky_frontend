734e75ae0ba68841afd659a2a47fd2762fdf65fd
HiImConan <mindol0320@naver.com>
2023-11-07 20:43:07 +0900

    docs: eslintrc 규칙 추가

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
@@ -9,6 +9,6 @@ export default defineConfig({
     port: 3000,
   },
   resolve: {
-    alias: { '@': '/src'}
+    alias: { find: '@', replacement: '/src' },
   },
 });
