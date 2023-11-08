1836f31026dd411c1c4d7e63d76318928c4d8057
pjh <pjh402941@gmail.com>
2023-11-08 18:08:52 +0900

    style: 로그인 페이지 컴포넌트 추가

--- src/routes/auth/AuthStyle.js
+++ src/routes/auth/AuthStyle.js
@@ -1,1 +1,35 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  position: relative;
+  text-align: center;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  /* 미디어 쿼리 적용 */
+  /* pc화면에서 너비를 390로 고정합니다*/
+  @media (hover: hover) {
+    width: 390px;
+    margin: 0 auto;
+  }
+  /* 스크롤바 숨기기 */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const Top = styled.div``;
+export const Bottom = styled.div``;
+export const ContentWrapper = styled.div`
+  margin: 0 auto;
+`;
+export const LogoImage = styled.div`
+  width: 195px;
+`;
+export const Caption = styled.div`
+  margin-top: 9px;
+  text-align: left;
+  margin-left: 10px;
+`;
