aaf8f56da2139edb029dd53de3444e9da0ff865c
pjh <pjh402941@gmail.com>
2023-11-08 18:34:11 +0900

    style: 플로깅 페이지 컴포넌트 추가

--- src/routes/plogging/index.jsx
+++ src/routes/plogging/index.jsx
@@ -1,5 +1,30 @@
-import KakaoMap from '@/components/kakao-map';
+import logo from '@/assets/icons/logo.png';
+import startButton from '@/assets/icons/buttons/startButton.png';
+import Navbar from '../../components/navigationBar';
+import Trash from '@/components/plogging/trash';
+import Timer from '@/components/plogging/timer';
+
+import {
+  Container,
+  ContentWrapper,
+  LogoImage,
+  GoPlogging,
+} from './PloggingStyle';
 
 export default function Plogging() {
-  return <KakaoMap />;
+  return (
+    <Container>
+      <ContentWrapper>
+        <LogoImage>
+          <img src={logo} alt="Logo" width="120px" height="64px" />
+        </LogoImage>
+        <GoPlogging>
+          <img src={startButton} width="312px" alt="start" />
+        </GoPlogging>
+        <Trash />
+        <Timer />
+      </ContentWrapper>
+      <Navbar />
+    </Container>
+  );
 }
