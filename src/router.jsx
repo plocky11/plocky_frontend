d863fe6de1e46fa65f6d94c0ef47118b292b766e
HiImConan <mindol0320@naver.com>
2023-11-08 19:48:14 +0900

    feat: router 분기 및 withAuth 조건 추가

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,48 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '@/routes/main';
+import RedirectedKakao from '@/routes/auth/kakao';
+import Auth from '@/routes/auth';
+import GeneralLayout from './components/layout';
 
-export const router = createBrowserRouter([
+const routerData = [
   {
     path: '/',
     element: <Main />,
+    withAuth: true,
   },
-]);
+  {
+    path: '/auth',
+    element: <Auth />,
+    withAuth: false,
+  },
+  {
+    path: '/auth/kakao',
+    element: <RedirectedKakao />,
+    withAuth: false,
+  },
+  {
+    path: '/plogging',
+    element: <div>추가 예정</div>,
+    withAuth: true,
+  },
+];
+
+export const routers = createBrowserRouter(
+  routerData.map(router => {
+    if (router.withAuth) {
+      return {
+        path: router.path,
+        element: (
+          <GeneralLayout withAuth={router.withAuth}>
+            {router.element}
+          </GeneralLayout>
+        ),
+      };
+    }
+
+    return {
+      path: router.path,
+      element: router.element,
+    };
+  }),
+);
