a4fde44be85c0853e95d5c4d15c9e9186c2b45ad
pjh <pjh402941@gmail.com>
2023-11-08 18:34:11 +0900

    style: 플로깅 페이지 컴포넌트 추가

---
+++ src/routes/plogging/PloggingStyle.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  position: relative;
+  text-align: center;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  /* 미디어 쿼리 적용 */
+  /* pc화면에서 너비를 390로 고정합니다*/
+  @media (hover: hover) {
+    width: 390px;
+    margin: 0 auto;
+  }
+  /* 스크롤바 숨기기 */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const ContentWrapper = styled.div`
+  margin-top: 2rem;
+`;
+export const LogoImage = styled.div`
+  margin-top: 2rem;
+`;
+
+export const GoPlogging = styled.div`
+  drop-shadow(0px 4px 8px 0px rgba(42, 0, 0, 0.1));
+`;
---
+++ src/routes/plogging/index.jsx
@@ -0,0 +1,25 @@
+import logo from '@/assets/icons/logo.png';
+import startButton from '@/assets/icons/buttons/startButton.png';
+import Navbar from '../../components/navigation-bar';
+import {
+  Container,
+  ContentWrapper,
+  LogoImage,
+  GoPlogging,
+} from './PloggingStyle';
+
+export default function Plogging() {
+  return (
+    <Container>
+      <ContentWrapper>
+        <LogoImage>
+          <img src={logo} alt="Logo" width="120px" height="64px" />
+        </LogoImage>
+        <GoPlogging>
+          <img src={startButton} width="312px" />
+        </GoPlogging>
+      </ContentWrapper>
+      <Navbar></Navbar>
+    </Container>
+  );
+}
