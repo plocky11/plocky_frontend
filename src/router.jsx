e82e9222871bceb840ebe306ab966f8dd7e18a97
pjh <pjh402941@gmail.com>
2023-11-08 18:32:02 +0900

    feat: 라우터 생성

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,20 @@
+import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '@/routes/main/index';
+import Auth from '@/routes/auth/index';
+import Plogging from './routes/plogging';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
+  },
+  {
+    path: '/login',
+    element: <Auth />,
+  },
+  {
+    path: '/plogging',
+    element: <Plogging />,
   },
 ]);
