8de36693526dcf95213a6ae70c51eed05ee7b8be
pjh <pjh402941@gmail.com>
2023-11-08 17:30:26 +0900

    feat: 새로운 에셋 추가, chore: prettier 에러 수정

--- src/assets/icons/characters/index.js
+++
@@ -1,2 +0,0 @@
-export { default as temp1 } from './temp1.png';
-export { default as temp2 } from './temp2.png';
--- src/assets/icons/markers/index.js
+++
@@ -1,2 +0,0 @@
-export { default as trashCanMarker } from './trashCanMarker.png';
-export { default as trashCanMarkerClosing } from './trashCanMarkerClosing.png';
