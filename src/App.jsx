db3a02253c8af46a7e06ab52634e04ec796563a0
HiImConan <mindol0320@naver.com>
2023-11-08 19:48:14 +0900

    feat: router 분기 및 withAuth 조건 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { router } from '@/router';
+import { routers } from '@/router';
 import theme from '@/styles/theme';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <RouterProvider router={routers} />
     </ThemeProvider>
   );
 }
