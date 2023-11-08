a4ef57fa8a86e167547000f469b3d3200baa7b84
HiImConan <mindol0320@naver.com>
2023-11-08 19:55:04 +0900

    refactor: UserProvider GeneralLayout에 추가

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,33 +1,21 @@
-// import { useCallback, useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { UserProvider } from '@/api/context/userContext';
 
 export default function GeneralLayout({ children, withAuth }) {
-  // const [userProfile, setUserProfile] = useRecoilState(UserAtom);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // const fetchUserProfile = useCallback(async () => {
-  //   const userProfileResponse = await getCurrentUserInfo();
+  // 인증이 필요한 페이지에서 로그인하지 않은 user는 로그인 페이지로 이동
+  if (withAuth && !localStorage.getItem('access_token')) {
+    navigate('/login');
+    return <>403: 권한 없음</>;
+  }
 
-  //   if (!userProfileResponse) {
-  //     navigate('/login');
-  //     return;
-  //   }
-
-  //   setUserProfile(userProfileResponse);
-  // }, []);
-
-  // useEffect(() => {
-  //   fetchUserProfile();
-  // }, [children]);
-
-  // // 인증이 필요한 페이지에서 로그인하지 않은 user는 로그인 페이지로 이동
-  // if (withAuth && userProfile?.userInfo.roles.length === 0) {
-  //   navigate('/login');
-  //   return <></>;
-  // }
-
-  return <GeneralLayoutWrapper>{children}</GeneralLayoutWrapper>;
+  return (
+    <UserProvider>
+      <GeneralLayoutWrapper>{children}</GeneralLayoutWrapper>
+    </UserProvider>
+  );
 }
 
 const GeneralLayoutWrapper = styled.div`
