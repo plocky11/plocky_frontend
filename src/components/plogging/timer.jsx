99bcf21ac743bf11b4f4d02218fe0e5b16102bd1
pjh <pjh402941@gmail.com>
2023-11-10 10:31:47 +0900

    feat: 타이머 컴포넌트 생성

--- src/components/plogging/timer.jsx
+++ src/components/plogging/timer.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-
+const TimerWrapper = styled.div`
+  width: 104px;
+  height: 40px;
+  border-radius: 16px;
+  border: 2px solid #2a6d0d;
+  background: var(--green-background, #fdfff5);
+  display: flex;
+  justify-content: center;
+  color: ${({ theme }) => theme.palette.green.darker};
+  font-size: 1.5rem;
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  box-sizing: border-box;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  bottom: 33vh;
+`;
 function Timer() {
-  return <div>어쩌고</div>;
+  return (
+    <>
+      <TimerWrapper>00:00</TimerWrapper>
+    </>
+  );
 }
 
 export default Timer;
