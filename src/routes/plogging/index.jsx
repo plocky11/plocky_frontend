b9468d35cbcf7d0eb0515fc7c9c898bb135a5e4c
pjh <pjh402941@gmail.com>
2023-11-10 10:36:03 +0900

    chore: 플로깅 페이지에 컴포넌트 넣음

--- src/routes/plogging/index.jsx
+++ src/routes/plogging/index.jsx
@@ -1,30 +1,53 @@
+import React, { useState } from 'react';
 import logo from '@/assets/icons/logo.png';
 import startButton from '@/assets/icons/buttons/startButton.png';
 import Navbar from '../../components/navigationBar';
 import Trash from '@/components/plogging/trash';
 import Timer from '@/components/plogging/timer';
+// import AddTrashcanMarker from '@/components/plogging/addTrashcanMarker';
 
 import {
   Container,
   ContentWrapper,
-  LogoImage,
   GoPlogging,
+  LogoImage,
 } from './PloggingStyle';
 
 export default function Plogging() {
+  const [isPlogging, setIsPlogging] = useState(false);
+
+  const handleStartPlogging = () => {
+    setIsPlogging(true);
+  };
+
+  const handleEndPlogging = () => {
+    //  완료 화면으로 이동
+  };
+
+  const contentWrapperStyle = !isPlogging
+    ? { height: 'calc(100vh - 62px)' }
+    : { height: '100vh' };
+
   return (
     <Container>
-      <ContentWrapper>
+      <ContentWrapper style={contentWrapperStyle}>
         <LogoImage>
           <img src={logo} alt="Logo" width="120px" height="64px" />
         </LogoImage>
-        <GoPlogging>
-          <img src={startButton} width="312px" alt="start" />
-        </GoPlogging>
-        <Trash />
-        <Timer />
+        {/* <AddTrashcanMarker /> */}
+        {!isPlogging && (
+          <GoPlogging onClick={handleStartPlogging}>
+            <img src={startButton} width="312px" alt="start" />
+          </GoPlogging>
+        )}
+        {isPlogging && (
+          <>
+            <Trash />
+            <Timer />
+          </>
+        )}
       </ContentWrapper>
-      <Navbar />
+      {!isPlogging && <Navbar />}
     </Container>
   );
 }
