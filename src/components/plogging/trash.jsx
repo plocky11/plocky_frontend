073a820312a47aa1324c2e4215e666b1cec1edfd
pjh <pjh402941@gmail.com>
2023-11-10 10:33:54 +0900

    chore: 쓰레기개수 컴포넌트 수정

--- src/components/plogging/trash.jsx
+++ src/components/plogging/trash.jsx
@@ -11,10 +11,43 @@ import minus from '@/assets/icons/plogging/minus.png';
 import plus from '@/assets/icons/plogging/plus.png';
 import square from '@/assets/icons/background/square.png';
 import squareLine from '@/assets/icons/background/squareLine.png';
+import endButton from '@/assets/icons/buttons/endButton.png';
+import trashcanButton from '@/assets/icons/buttons/trashcanButton.png';
+import trashcanAdd from '@/assets/icons/buttons/trashcanAdd.png';
+import trashcanDelete from '@/assets/icons/buttons/trashcanDelete.png';
 
-const Container = styled.div`
+const SquareWrapper = styled.div`
+  width: 100vw;
+  position: absolute;
+  bottom: 0;
+  @media (hover: hover) {
+    width: 390px;
+    margin: 0 auto;
+  }
+`;
+const FunctionWrapper = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: 30vh;
   width: 100%;
+  padding: 0 1.5rem;
+  box-sizing: border-box;
+  justify-content: space-between;
+  margin-bottom: 0.8rem;
+`;
+const Trashcan = styled.div`
   position: relative;
+`;
+
+const ClickableFunction1 = styled.div`
+  position: absolute;
+  top: 6px;
+  left: 14px;
+`;
+const ClickableFunction2 = styled.div`
+  position: absolute;
+  top: 5px;
+  right: 11px;
 `;
 
 const Line = styled.div`
@@ -91,109 +124,125 @@ function Trash() {
   };
 
   return (
-    <Container>
-      <img
-        src={square}
-        style={{ width: '100%', height: '30vh', minHeight: '207px' }}
-        alt="trash"
-      />
-      <Line>
-        <img src={squareLine} width="104px" alt="line" />
-      </Line>
-      <ItemWrapper>
-        <ItemBox>
-          <ClickableItem onClick={() => handleMinusClick('cigarette')}>
-            <img src={minus} width="18px" alt="minus" />
-          </ClickableItem>
-          <Item>
-            <img src={cigarette} width="24px" alt="cigarette" />
-            <span>담배꽁초</span>
-            <Num>{counts.cigarette}</Num>
-          </Item>
-          <ClickableItem onClick={() => handlePlusClick('cigarette')}>
-            <img src={plus} width="18px" alt="plus" />
-          </ClickableItem>
-        </ItemBox>
-        <ItemBox>
-          <ClickableItem onClick={() => handleMinusClick('plastic')}>
-            <img src={minus} width="18px" id="minus" alt="minus" />
-          </ClickableItem>
-          <Item>
-            <img src={plastic} width="24px" alt="plastic" />
-            <span>플라스틱</span>
-            <Num>{counts.plastic}</Num>
-          </Item>
-          <ClickableItem onClick={() => handlePlusClick('plastic')}>
-            <img src={plus} width="18px" id="plus" alt="plus" />
-          </ClickableItem>
-        </ItemBox>
-        <ItemBox>
-          <ClickableItem onClick={() => handleMinusClick('can')}>
-            <img src={minus} width="18px" id="minus" alt="minus" />
-          </ClickableItem>
-          <Item>
-            <img src={can} width="24px" alt="can" />
-            <span>캔</span>
-            <Num>{counts.can}</Num>
-          </Item>
-          <ClickableItem onClick={() => handlePlusClick('can')}>
-            <img src={plus} width="18px" id="plus" alt="plus" />
-          </ClickableItem>
-        </ItemBox>
-        <ItemBox>
-          <ClickableItem onClick={() => handleMinusClick('paper')}>
-            <img src={minus} width="18px" id="minus" alt="minus" />
-          </ClickableItem>
-          <Item>
-            <img src={paper} width="24px" alt="paper" />
-            <span>종이</span>
-            <Num>{counts.paper}</Num>
-          </Item>
-          <ClickableItem onClick={() => handlePlusClick('paper')}>
-            <img src={plus} width="18px" id="plus" alt="plus" />
-          </ClickableItem>
-        </ItemBox>
-        <ItemBox>
-          <ClickableItem onClick={() => handleMinusClick('glass')}>
-            <img src={minus} width="18px" id="minus" alt="minus" />
-          </ClickableItem>
-          <Item>
-            <img src={glass} width="24px" alt="glass" />
-            <span>유리</span>
-            <Num>{counts.glass}</Num>
-          </Item>
-          <ClickableItem onClick={() => handlePlusClick('glass')}>
-            <img src={plus} width="18px" id="plus" alt="plus" />
-          </ClickableItem>
-        </ItemBox>
-        <ItemBox>
-          <ClickableItem onClick={() => handleMinusClick('vinyl')}>
-            <img src={minus} width="18px" id="minus" alt="minus" />
-          </ClickableItem>
-          <Item>
-            <img src={vinyl} width="24px" alt="vinyl" />
-            <span>비닐</span>
-            <Num>{counts.vinyl}</Num>
-          </Item>
-          <ClickableItem onClick={() => handlePlusClick('vinyl')}>
-            <img src={plus} width="18px" id="plus" alt="plus" />
-          </ClickableItem>
-        </ItemBox>
-        <ItemBox>
-          <ClickableItem onClick={() => handleMinusClick('others')}>
-            <img src={minus} width="18px" id="minus" alt="minus" />
-          </ClickableItem>
-          <Item>
-            <img src={others} width="24px" alt="others" />
-            <span>기타</span>
-            <Num>{counts.others}</Num>
-          </Item>
-          <ClickableItem onClick={() => handlePlusClick('others')}>
-            <img src={plus} width="18px" id="plus" alt="plus" />
-          </ClickableItem>
-        </ItemBox>
-      </ItemWrapper>
-    </Container>
+    <>
+      <FunctionWrapper>
+        <div>
+          <img src={endButton} width="72px" height="40px" alt="end" />
+        </div>
+        <Trashcan>
+          <img src={trashcanButton} width="64px" alt="trashcan" />
+          <ClickableFunction1>
+            <img src={trashcanAdd} width="15px" alt="add" />
+          </ClickableFunction1>
+          <ClickableFunction2>
+            <img src={trashcanDelete} width="22px" alt="delete" />
+          </ClickableFunction2>
+        </Trashcan>
+      </FunctionWrapper>
+      <SquareWrapper>
+        <img
+          src={square}
+          style={{ width: '100%', height: '30vh', minHeight: '207px' }}
+          alt="trash"
+        />
+        <Line>
+          <img src={squareLine} width="104px" alt="line" />
+        </Line>
+        <ItemWrapper>
+          <ItemBox>
+            <ClickableItem onClick={() => handleMinusClick('cigarette')}>
+              <img src={minus} width="18px" alt="minus" />
+            </ClickableItem>
+            <Item>
+              <img src={cigarette} width="24px" alt="cigarette" />
+              <span>담배꽁초</span>
+              <Num>{counts.cigarette}</Num>
+            </Item>
+            <ClickableItem onClick={() => handlePlusClick('cigarette')}>
+              <img src={plus} width="18px" alt="plus" />
+            </ClickableItem>
+          </ItemBox>
+          <ItemBox>
+            <ClickableItem onClick={() => handleMinusClick('plastic')}>
+              <img src={minus} width="18px" id="minus" alt="minus" />
+            </ClickableItem>
+            <Item>
+              <img src={plastic} width="24px" alt="plastic" />
+              <span>플라스틱</span>
+              <Num>{counts.plastic}</Num>
+            </Item>
+            <ClickableItem onClick={() => handlePlusClick('plastic')}>
+              <img src={plus} width="18px" id="plus" alt="plus" />
+            </ClickableItem>
+          </ItemBox>
+          <ItemBox>
+            <ClickableItem onClick={() => handleMinusClick('can')}>
+              <img src={minus} width="18px" id="minus" alt="minus" />
+            </ClickableItem>
+            <Item>
+              <img src={can} width="24px" alt="can" />
+              <span>캔</span>
+              <Num>{counts.can}</Num>
+            </Item>
+            <ClickableItem onClick={() => handlePlusClick('can')}>
+              <img src={plus} width="18px" id="plus" alt="plus" />
+            </ClickableItem>
+          </ItemBox>
+          <ItemBox>
+            <ClickableItem onClick={() => handleMinusClick('paper')}>
+              <img src={minus} width="18px" id="minus" alt="minus" />
+            </ClickableItem>
+            <Item>
+              <img src={paper} width="24px" alt="paper" />
+              <span>종이</span>
+              <Num>{counts.paper}</Num>
+            </Item>
+            <ClickableItem onClick={() => handlePlusClick('paper')}>
+              <img src={plus} width="18px" id="plus" alt="plus" />
+            </ClickableItem>
+          </ItemBox>
+          <ItemBox>
+            <ClickableItem onClick={() => handleMinusClick('glass')}>
+              <img src={minus} width="18px" id="minus" alt="minus" />
+            </ClickableItem>
+            <Item>
+              <img src={glass} width="24px" alt="glass" />
+              <span>유리</span>
+              <Num>{counts.glass}</Num>
+            </Item>
+            <ClickableItem onClick={() => handlePlusClick('glass')}>
+              <img src={plus} width="18px" id="plus" alt="plus" />
+            </ClickableItem>
+          </ItemBox>
+          <ItemBox>
+            <ClickableItem onClick={() => handleMinusClick('vinyl')}>
+              <img src={minus} width="18px" id="minus" alt="minus" />
+            </ClickableItem>
+            <Item>
+              <img src={vinyl} width="24px" alt="vinyl" />
+              <span>비닐</span>
+              <Num>{counts.vinyl}</Num>
+            </Item>
+            <ClickableItem onClick={() => handlePlusClick('vinyl')}>
+              <img src={plus} width="18px" id="plus" alt="plus" />
+            </ClickableItem>
+          </ItemBox>
+          <ItemBox>
+            <ClickableItem onClick={() => handleMinusClick('others')}>
+              <img src={minus} width="18px" id="minus" alt="minus" />
+            </ClickableItem>
+            <Item>
+              <img src={others} width="24px" alt="others" />
+              <span>기타</span>
+              <Num>{counts.others}</Num>
+            </Item>
+            <ClickableItem onClick={() => handlePlusClick('others')}>
+              <img src={plus} width="18px" id="plus" alt="plus" />
+            </ClickableItem>
+          </ItemBox>
+        </ItemWrapper>
+      </SquareWrapper>
+    </>
   );
 }
 
