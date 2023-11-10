daf00dd6d2a182ff2f27d2798bbd2efb665b366c
pjh <pjh402941@gmail.com>
2023-11-10 10:33:03 +0900

    feat: 마커 추가 컴포넌트 생성

---
+++ src/components/plogging/addTrashcanMarker.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+const MarkerWrapper = styled.div`
+  width: 21rem;
+  height: 3rem;
+  flex-shrink: 0;
+  color: #fdfff5;
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  border-radius: 1rem;
+  background: ${({ theme }) => theme.palette.green.light};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+function AddTrashcanMarker() {
+  return (
+    <>
+      <MarkerWrapper>마커를 추가할 위치를 클릭해주세요</MarkerWrapper>
+    </>
+  );
+}
+
+export default AddTrashcanMarker;
