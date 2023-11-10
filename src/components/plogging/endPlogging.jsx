9e755cb0d12a086a23c4462680eb1dbf76a7bc08
pjh <pjh402941@gmail.com>
2023-11-10 10:34:59 +0900

    chore: 플로깅 종료 모달 파일 생성

---
+++ src/components/plogging/endPlogging.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ModalWrapper = styled.div``;
+function EndPlogging() {
+  return (
+    <>
+      <ModalWrapper />
+    </>
+  );
+}
+
+export default EndPlogging;
