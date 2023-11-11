e3e61a688fbe0431e50587db7f12831ffdb14a68
HiImConan <mindol0320@naver.com>
2023-11-12 00:54:14 +0900

    feat: watchPosition()으로 위치 로깅 추가

--- src/components/kakao-map/index.jsx
+++ src/components/kakao-map/index.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, Polyline } from 'react-kakao-maps-sdk';
 import useKakaoMapLoader from './useKakaoMapLoader';
 import trashCanMarker from '@/assets/icons/markers/trashCanMarker.png';
 import locationMarker from '@/assets/icons/markers/locationMarker.png';
-import instance from '@/api/auth';
+import {
+  usePloggingActions,
+  usePloggingStart,
+  usePloggingValue,
+} from '@/api/context/ploggingContext';
+import theme from '@/styles/theme';
 
 export default function KakaoMap() {
   useKakaoMapLoader();
@@ -16,18 +21,40 @@ export default function KakaoMap() {
     isLoading: true,
   });
   const [positions, setPositions] = useState([]);
+  const add = usePloggingActions();
+  const ploggingData = usePloggingValue();
+  const [isPloggingStarted] = usePloggingStart();
+
+  const watcher = navigator.geolocation.watchPosition(
+    position => {
+      setLocation(prev => ({
+        ...prev,
+        center: {
+          lat: position.coords.latitude, // 위도
+          lng: position.coords.longitude, // 경도
+        },
+        isLoading: false,
+      }));
+      add({
+        lat: position.coords.latitude, // 위도
+        lng: position.coords.longitude, // 경도
+      });
+    },
+    err => {
+      setLocation(prev => ({
+        ...prev,
+        errMsg: err.message,
+        isLoading: false,
+      }));
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 4000,
+      maximumAge: 0,
+    },
+  );
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await instance.get('/trash');
-        setPositions(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error fetching trash can data:', error);
-      }
-    };
-
     if (navigator.geolocation) {
       // GeoLocation을 이용해서 접속 위치를 얻어옵니다
       navigator.geolocation.getCurrentPosition(
@@ -40,6 +67,10 @@ export default function KakaoMap() {
             },
             isLoading: false,
           }));
+          add({
+            lat: position.coords.latitude, // 위도
+            lng: position.coords.longitude, // 경도
+          });
         },
         err => {
           setLocation(prev => ({
@@ -48,8 +79,12 @@ export default function KakaoMap() {
             isLoading: false,
           }));
         },
+        {
+          enableHighAccuracy: false,
+          timeout: 4000,
+          maximumAge: 0,
+        },
       );
-      fetchData();
     } else {
       // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
       setLocation(prev => ({
@@ -59,6 +94,17 @@ export default function KakaoMap() {
       }));
     }
   }, []);
+
+  const cleanUp = () => {
+    navigator.geolocation && navigator.geolocation.clearWatch(watcher);
+    localStorage.removeItem('plogging_path');
+  };
+
+  useEffect(() => {
+    if (isPloggingStarted === true) watcher;
+
+    return cleanUp;
+  }, [isPloggingStarted]);
 
   return (
     <Map
@@ -90,6 +136,15 @@ export default function KakaoMap() {
           }, // 마커이미지의 크기입니다
         }}
       />
+      {ploggingData && isPloggingStarted === true && (
+        <Polyline
+          path={[ploggingData]}
+          strokeWeight={8} // 선의 두께 입니다
+          strokeColor={theme.palette.green.dark} // 선의 색깔입니다
+          strokeOpacity={0.7} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+          strokeStyle={'solid'} // 선의 스타일입니다
+        />
+      )}
     </Map>
   );
 }
