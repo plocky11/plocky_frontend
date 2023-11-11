0362fe2cd3a4ab551eff3f841eab98d3fb6919df
HiImConan <mindol0320@naver.com>
2023-11-11 21:48:42 +0900

    feat: plogging 관련 api 추가

---
+++ src/api/plogging.js
@@ -0,0 +1,23 @@
+import instance from './instance';
+
+export const addPlogging = async body => {
+  return await instance.post('/members/ploggings', body);
+};
+
+export const getSinglePloggingInfo = async ploggingId => {
+  const response = await instance.get(`/members/ploggins/${ploggindId}`);
+  return response.data;
+};
+
+export const getTrashcanList = async () => {
+  const response = await instance.get('/trashcan');
+  return response.data;
+};
+
+export const addNewTrashcan = async body => {
+  return await instance.post('/trashcan', body);
+};
+
+export const deleteTrashcan = async trashcanId => {
+  return await instance.delete('/trashcan', { trashcanId });
+};
